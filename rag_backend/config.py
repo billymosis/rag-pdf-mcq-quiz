cbe478bb06d91592a66b76cc1ccabd0b5d308073
Billy Priambodo <edo.billy@gmail.com>
2025-05-31 18:25:23 +0700

    feat: v4

--- rag_backend/config.py
+++ rag_backend/config.py
@@ -9,7 +9,7 @@ CHUNK_OVERLAP = 200
 
 # --- RAG Parameters ---
 TOP_K_RETRIEVAL = 8  # Number of most relevant documents to retrieve
-LLM_MODEL_NAME = "gemini-1.5-flash"
+LLM_MODEL_NAME = "gemini-2.5-flash-preview-05-20"
 LLM_MODEL_EMBEDDING = "models/text-embedding-004"
 # LLM_MODEL_EMBEDDING = "gemini-embedding-exp-03-07"
 LLM_TEMPERATURE = 0.0  # Set to 0.0 for deterministic answers
